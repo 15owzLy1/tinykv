3ce6edb227928046b77a56d53c94971b772d218c
C0D1NGK1NG <1837639445@qq.com>
2024-10-22 21:47:26 +0800

    project1 finished

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 )
 
 // The functions below are Server's Raw API. (implements TinyKvServer).
@@ -11,26 +12,113 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	// 创建一个新的读事务
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()  // 确保读事务在结束时关闭
+
+	// 从指定列族 (CF) 获取键的值
+	val, err := reader.GetCF(req.Cf, req.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	// 构建响应，根据是否存在值设置 NotFound 字段
+	resp := &kvrpcpb.RawGetResponse{
+		Value:    val,
+		NotFound: val == nil,
+	}
+
+	return resp, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+    // 创建一个 Modify 对象表示要存储的数据
+    put := storage.Modify{
+        Data: storage.Put{
+            Cf:    req.Cf,
+            Key:   req.Key,
+            Value: req.Value,
+        },
+    }
+
+    // 使用 Storage 接口的 Write 方法将数据写入存储
+    err := server.storage.Write(req.Context, []storage.Modify{put})
+    if err != nil {
+        return nil, err
+    }
+
+    // 构建响应
+    return &kvrpcpb.RawPutResponse{}, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+    // 创建一个 Modify 对象表示要删除的数据
+    del := storage.Modify{
+        Data: storage.Delete{
+            Cf:  req.Cf,
+            Key: req.Key,
+        },
+    }
+
+    // 使用 Storage 接口的 Write 方法删除数据
+    err := server.storage.Write(req.Context, []storage.Modify{del})
+    if err != nil {
+        return nil, err
+    }
+
+    // 构建响应
+    return &kvrpcpb.RawDeleteResponse{}, nil
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+    // 创建一个新的读事务
+    reader, err := server.storage.Reader(req.Context)
+    if err != nil {
+        return nil, err
+    }
+    defer reader.Close()  // 确保读事务在结束时关闭
+
+    // 创建迭代器，开始从指定的 start key 扫描
+    iter := reader.IterCF(req.Cf)
+    defer iter.Close()
+
+    var kvPairs []*kvrpcpb.KvPair
+    for iter.Seek(req.StartKey); iter.Valid(); iter.Next() {
+        item := iter.Item()
+        key := item.Key()
+
+        // 获取值
+        val, err := item.ValueCopy(nil)
+        if err != nil {
+            return nil, err
+        }
+
+        // 构造键值对
+        kvPairs = append(kvPairs, &kvrpcpb.KvPair{
+            Key:   key,
+            Value: val,
+        })
+
+        // 达到限制时停止
+        if len(kvPairs) >= int(req.Limit) {
+            break
+        }
+    }
+
+    // 构建响应
+    return &kvrpcpb.RawScanResponse{
+        Kvs: kvPairs,
+    }, nil
 }
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -4,17 +4,21 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
+	"github.com/Connor1996/badger"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	db *badger.DB
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+    db := engine_util.CreateDB(conf.DBPath, false)
+    return &StandAloneStorage{db: db}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -24,15 +28,57 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
-	return nil
+	return s.db.Close()
+}
+
+type StandAloneStorageReader struct {
+    txn *badger.Txn
+}
+
+// GetCF 使用事务 txn 从指定列族 (CF) 中获取键值
+func (r *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+    item, err := r.txn.Get(engine_util.KeyWithCF(cf, key))
+    if err != nil {
+        if err == badger.ErrKeyNotFound {
+            return nil, nil
+        }
+        return nil, err
+    }
+    val, err := item.ValueCopy(nil)
+    return val, err
+}
+
+func (r *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+    return engine_util.NewCFIterator(cf, r.txn)
+}
+
+func (r *StandAloneStorageReader) Close() {
+    r.txn.Discard()
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	txn := s.db.NewTransaction(false)  // 创建只读事务
+    return &StandAloneStorageReader{txn: txn}, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Your Code Here (1).
-	return nil
+    txn := s.db.NewTransaction(true)  // 创建写事务
+    defer txn.Discard()
+
+    for _, m := range batch {
+        switch data := m.Data.(type) {
+        case storage.Put:
+            err := txn.Set(engine_util.KeyWithCF(data.Cf, data.Key), data.Value)
+            if err != nil {
+                return err
+            }
+        case storage.Delete:
+            err := txn.Delete(engine_util.KeyWithCF(data.Cf, data.Key))
+            if err != nil {
+                return err
+            }
+        }
+    }
+    return txn.Commit()
 }
